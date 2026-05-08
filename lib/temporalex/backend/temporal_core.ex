924636dbee2bb4bf00b483f71e399b5b274125ab
Hans Josephsen <hans@bukholm.com>
2026-05-09 00:12:27 +0200

    add real temporal client semantics coverage

--- lib/temporalex/backend/temporal_core.ex
+++ lib/temporalex/backend/temporal_core.ex
@@ -398,7 +398,13 @@ def complete_activity_task(%WorkerState{} = state, %ActivityCompletion{} = compl
 
   defp native_headers_opts(opts) do
     opts
-    |> Keyword.take([:headers, :request_id, :update_id])
+    |> Keyword.take([
+      :headers,
+      :request_id,
+      :update_id,
+      :query_reject_condition,
+      :reject_condition
+    ])
     |> normalize_native_opts()
   end
 
